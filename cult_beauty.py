09c9302ded0eb1787fa1cdd9f01bd44eb71ffe76
Eslam Allam <86079760+eslam-allam@users.noreply.github.com>
2023-08-12 21:04:04 +0300

    feat: finished all features from test scrape meeting

--- cult_beauty.py
+++ cult_beauty.py
@@ -74,12 +74,34 @@ def safe_get_element(wd: webdriver.WebDriver, by: By, value:str):
         return element
     except NoSuchElementException:
         return None
-    
+
+def change_currency(wd: webdriver.WebDriver, to = '£ (GBP)'):
+    try:
+        settings_button = wait_for_presence_get(wd ,By.CLASS_NAME, 'responsiveSubMenu_sessionSettings', 10)
+        if settings_button is None:
+            logger.fatal('Could not locate settings button to change currency.')
+            return False
+        click_element_refresh_stale(wd, settings_button, By.CLASS_NAME, 'responsiveSubMenu_sessionSettings')
+        currency_select_button = wait_for_presence_get(wd ,By.CLASS_NAME, 'sessionSettings_currencySelect')
+        if currency_select_button is None:
+            logger.fatal('Could not locate currency select list to change currency.')
+            return False
+        Select(currency_select_button).select_by_visible_text(to)
+        save_button = wait_for_presence_get(wd ,By.CLASS_NAME, 'sessionSettings_saveButton')
+        if save_button is None:
+            logger.fatal('Could not locate save button to change currency.')
+            return False
+        click_element_refresh_stale(wd, save_button, By.CLASS_NAME, 'sessionSettings_saveButton')
+        time.sleep(2)
+        return True
+    except Exception:
+        logger.fatal('An unexpected error occurred while changing currency.', exc_info=True)
+        return False
 
 def click_element_refresh_stale(wd: webdriver.WebDriver, element: WebElement, by: By, locator: str, index = None):
     while True:
         try:
-            wd.execute_script("arguments[0].click();", element)
+            wd.execute_script(JAVASCRIPT_EXECUTE_CLICK, element)
             return element
         except Exception:
             logger.debug('Could not click element. Refreshing...')
@@ -144,14 +166,17 @@ def get_variation_images(wd: webdriver.WebDriver, variation_details:dict[str, ob
         variation_details[f'product_image_{i+1}'] = image_src
     return variation_details
 
-def wait_for_presence_get(wd: webdriver.WebDriver, by: By, value: str, wait_for: int = 2):
+def wait_for_presence_get(wd: webdriver.WebDriver, by: By, value: str, wait_for: int = 2, must_be_visible = False):
     try:
-        WebDriverWait(wd, wait_for).until(EC.presence_of_element_located((by, value)))
+        wait_condition = EC.presence_of_element_located((by, value))
+        if must_be_visible:
+            wait_condition = EC.visibility_of_element_located((by, value))
+        WebDriverWait(wd, wait_for).until(wait_condition)
     except TimeoutException:
         return None
     return wd.find_element(by, value)
 
-def get_variation_misc_details(wd: webdriver.WebDriver, variation_details:dict[str, object], product_id: str):
+def get_variation_misc_details(wd: webdriver.WebDriver, variation_details:dict[str, object], product_id: str, force_out_of_stock = False):
     variation_details['variant_SKU'] = product_id
     product_name = wait_for_presence_get(wd, By.CLASS_NAME, 'productName_title')
     variation_details['product_name'] = get_attribute_retry_stale(wd, product_name, 'textContent', variation_details
@@ -182,7 +207,10 @@ def get_variation_misc_details(wd: webdriver.WebDriver, variation_details:dict[s
         wd.find_element(By.CLASS_NAME, 'productAddToBasket-soldOut')
         variation_details['in_stock'] = 'no'
     except NoSuchElementException:
-        variation_details['in_stock'] = 'yes'
+        if force_out_of_stock:
+            variation_details['in_stock'] = 'no'
+        else:
+            variation_details['in_stock'] = 'yes'
     return variation_details
 
 def get_multi_size_details(wd: webdriver.WebDriver, product_details: dict[str, object]) -> list[dict[str, object]]:
@@ -194,7 +222,7 @@ def get_multi_size_details(wd: webdriver.WebDriver, product_details: dict[str, o
         is_selected = safe_get_element(button, By.CLASS_NAME, 'srf-hide')
         if is_selected is None:
             old_price = get_old_price(wd)
-            wd.execute_script('arguments[0].click();', button)
+            wd.execute_script(JAVASCRIPT_EXECUTE_CLICK, button)
             try:
                 WebDriverWait(wd, 10).until(EC.staleness_of(old_price))
             except Exception:
@@ -210,9 +238,9 @@ def get_multi_size_details(wd: webdriver.WebDriver, product_details: dict[str, o
         variations.append(variation_details)
     return variations
 
-def get_id_from_url(url:str):
+def get_id_from_url(url:str, first_splitter = '.', second_splitter = '-'):
     base_name = os.path.basename(urlsplit(url).path)
-    return base_name.split('.')[0].split('-')[0].strip()
+    return base_name.split(first_splitter)[0].split(second_splitter)[0].strip()
 
 def get_old_price(wd: webdriver.WebDriver):
     try:
@@ -244,10 +272,13 @@ def get_multi_color_details(wd: webdriver.WebDriver, product_details: dict[str, 
             variation_type = 'option'
         else:
             raise ValueError(f'Invalid product type: {product_type}')
-        variation_details[variation_type] = option
+        force_out_of_stock = False
+        if option.endswith('- Out of stock'):
+            force_out_of_stock = True
+        variation_details[variation_type] = option.removesuffix('- Out of stock').strip()
         variation_details = get_variation_images(wd, variation_details)
         product_id = get_id_from_url(variation_details['product_image_1'])
-        variation_details = get_variation_misc_details(wd, variation_details, product_id)
+        variation_details = get_variation_misc_details(wd, variation_details, product_id, force_out_of_stock)
 
         if product_type != ProductType.MULTI_OPTION:
             color = wd.find_element(By.CSS_SELECTOR, f"span[data-value-id='{id}']").value_of_css_property('background-color')
@@ -305,7 +336,7 @@ def get_product_descriptions(wd: webdriver.WebDriver, product_details: dict[str,
             button_id = button.get_attribute("id")
             is_expanded = button.get_attribute('aria-expanded')
             if is_expanded == 'false':
-                wd.execute_script("arguments[0].click();", button)
+                wd.execute_script(JAVASCRIPT_EXECUTE_CLICK, button)
             description_content = wd.find_element(By.ID, button_id.replace('heading', 'content')).text
             product_details[button.text] = description_content
         except ElementNotInteractableException:
@@ -316,7 +347,7 @@ def get_product_descriptions(wd: webdriver.WebDriver, product_details: dict[str,
     
     return product_details
 
-def get_product_details(wd:webdriver.WebDriver, urls: list[str], current_bar_position: int):
+def get_product_details(wd:webdriver.WebDriver, urls: list[str], product_category, current_bar_position: int):
     df = pd.DataFrame()
     # TODO add reset and leave = True
     for url in tqdm(urls, colour='green', position=current_bar_position + 1, desc='Products scanned', unit='Products', leave=False):
@@ -325,6 +356,7 @@ def get_product_details(wd:webdriver.WebDriver, urls: list[str], current_bar_pos
             product_details = {}
             product_variations = []
             product_details['product_url'] = url
+            product_details['product_category'] = product_category
             brand_element = safe_get_element(wd, By.CLASS_NAME, 'productBrandLogo_image')
 
             product_details['brand_name'] = brand_element.get_attribute('title') if brand_element is not None else None
@@ -349,13 +381,22 @@ def get_product_details(wd:webdriver.WebDriver, urls: list[str], current_bar_pos
 def get_category_links(browser_options: options.Options, url):
     worker = current_process()
     current_process_id = worker._identity[0]
-    category_name = get_id_from_url(url)
+    category_name = get_id_from_url(url, second_splitter=' ').replace('-', ' ')
     worker.name = f'WORKER#{current_process_id}_{category_name}'
     progress_bar_position = (current_process_id - 1) * 2
     with webdriver.WebDriver(browser_options) as wd:
-        time.sleep(ACTION_DELAY_SEC)
-        product_details = pd.DataFrame()
+        wd.implicitly_wait(ACTION_DELAY_SEC)
         wd.get(f'{url}')
+        time.sleep(ACTION_DELAY_SEC)
+        wait_for_presence_get(wd, By.ID, 'onetrust-accept-btn-handler', wait_for=10, must_be_visible=True).click()
+        wait_for_presence_get(wd ,By.CSS_SELECTOR, 
+                          'body > div.emailReengagement > div > div.emailReengagement_form_container > button > svg > path'
+                          , wait_for= 10, must_be_visible=True).click()
+        product_details = pd.DataFrame()
+        if not change_currency(wd, '€ (EUR)'):
+            logger.critical('Could not change currency for category: "%s". URL "%s". Skipping category...', category_name, url)
+            return product_details
+        logger.info('Currency changed successfully for category: "%s"', category_name)
         last_page = wait_for_presence_get(wd, By.CSS_SELECTOR, 'a.responsivePaginationButton.responsivePageSelector.responsivePaginationButton--last')
         if last_page is not None:
             last_page = get_attribute_retry_stale(wd, last_page, 'textContent', {}, By.CSS_SELECTOR, 
@@ -369,7 +410,7 @@ def get_category_links(browser_options: options.Options, url):
         for page in tqdm(range(1, last_page + 1), colour='red', position= progress_bar_position, desc='Pages scanned', unit='Pages', postfix = {'category': category_name}):
             wd.get(f'{url}?pageNumber={page}')
             product_links = list(set([x.find_element(By.CLASS_NAME, 'productBlock_link').get_attribute('href') for x in wd.find_elements(By.CLASS_NAME, 'productBlock_itemDetails_wrapper')]))
-            product_details = pd.concat([product_details, get_product_details(wd, product_links, progress_bar_position)], ignore_index=True)
+            product_details = pd.concat([product_details, get_product_details(wd, product_links, category_name, progress_bar_position)], ignore_index=True)
             time.sleep(ACTION_DELAY_SEC)
         return product_details
 
@@ -397,44 +438,102 @@ def order_serialized_columns(columns: list[str], regex = r'_(\d+)'):
 
     return ordered_columns
 
+def first_not_null(names: pd.Series):
+    return next((x for x in names if not pd.isna(x)), pd.NA)
+
+def ship_to_bahrain(text: str, pattern = r'we regret.+(?:middle east|bahrain)'):
+    if pd.isna(text):
+        return 'no'
+    found = re.search(pattern, text, flags=re.IGNORECASE)
+    if found is None:
+        return 'yes'
+    return 'no'
+
+def remove_pattern(text: str, pattern = r'we regret.+(?:middle east|bahrain)'):
+    if pd.isna(text):
+        return text
+    return re.sub(pattern, '', text, flags=re.IGNORECASE)
+
+def remove_brand_name(row: pd.Series):
+    if (any((pd.isna(x) for x in row.values))):
+        return row['product_name']
+    if not (row['product_name'].casefold().startswith(row['brand_name'].casefold())):
+        return row['product_name']
+    new_product_name = row['product_name'].removeprefix(row['brand_name']).strip()
+    return new_product_name
+
 def main():
     start_time = time.time()
     df = pd.DataFrame()
-    with ProcessPoolExecutor(max_workers=10, initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),)) as executor:
+    with ProcessPoolExecutor(max_workers=NUM_OF_WORKERS, initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),)) as executor:
+        
         results = executor.map(get_category_links, [browser_options for _ in CATEGORY_LINKS],CATEGORY_LINKS)
-    for result in results:
-        df = pd.concat([df, result], ignore_index=True)
-    logger.info(f'Total data-frame shape: {df.shape}')
-    logger.info('Renaming product_type column...')
-    df.rename({'product_type':'variant_type'}, inplace=True)
-    logger.info('Reordering columns...')
-    df = df.reindex(order_serialized_columns(df.columns), axis=1)
-    logger.info("Exporting excel with duplicates...")
-    df.to_excel('./test_cult_beauty_with_duplicates.xlsx', index=False)
+        
+        for result in results:
+            df = pd.concat([df, result], ignore_index=True)
+        logger.info(f'Total data-frame shape: {df.shape}')
+        logger.info('Renaming product_type column...')
+        df.rename({'product_type':'variant_type'}, inplace=True)
+        logger.info('Reordering columns...')
+        df = df.reindex(order_serialized_columns(df.columns), axis=1)
+        logger.info("Exporting excel with duplicates...")
+        df.to_excel('./test_cult_beauty_with_duplicates.xlsx', index=False)
 
-    logger.info("Removing duplicate entries...")
-    df.drop_duplicates(subset='variant_SKU', inplace=True, ignore_index=True)
-    logger.info('Total data-frame shape after deduplication: %s', df.shape)
-    mask = df['primary_SKU'] == df['variant_SKU']
-    transform = df.groupby('primary_SKU')['primary_SKU'].transform(create_serialized_sku, mask)
-    logger.info("Serializing primary SKU...")
-    df[['serialized_primary_SKU', 'is_variant_of']] = pd.DataFrame(transform.to_list(), columns=['serialized_primary_SKU', 'is_variant_of']
-                                                                , index=transform.index)
-    logger.info("Cleaning price column...")
-    df['price'] = df['price'].transform(lambda x: re.sub(r'[^\d.]', '', x))
+        logger.info("Removing duplicate entries...")
+        df.drop_duplicates(subset='variant_SKU', inplace=True, ignore_index=True)
 
-    logger.info("Dropping empty columns...")
-    df.dropna(axis=1, how='all', inplace=True)
-    logger.info("Exporting excel without duplicates...")
-    df.to_excel('./test_cult_beauty_without_duplicates.xlsx', index=False)
-    logger.info('Total execution time: %s', datetime.timedelta(seconds=time.time() - start_time))
+        logger.info('Total data-frame shape after deduplication: %s', df.shape)
+
+        mask = df['primary_SKU'] == df['variant_SKU']
+        transform = df.groupby('primary_SKU')['primary_SKU'].transform(create_serialized_sku, mask)
+        logger.info("Serializing primary SKU...")
+        df[['serialized_primary_SKU', 'is_variant_of']] = pd.DataFrame(transform.to_list(), columns=['serialized_primary_SKU', 'is_variant_of']
+                                                                    , index=transform.index)
+        
+        logger.info("Cleaning price column...")
+        df['price'] = df['price'].transform(lambda x: re.sub(r'[^\d.]', '', x))
+
+        logger.info("Dropping empty columns...")
+        df.dropna(axis=1, how='all', inplace=True)
+
+        logger.info('Removing refill options...')
+        combined_variants = df[['option', 'color', 'size', 'shade']].apply(first_not_null, axis= 1)
+        mask = df.loc[(~pd.isna(combined_variants)) & combined_variants.str.contains('refill', case=False)].index
+        df.drop(mask, inplace=True)
+
+        logger.info("Removing gift vouchers...")
+        mask = df.loc[(~pd.isna(combined_variants)) & combined_variants.str.contains('€', case=False)].index
+        df.drop(mask, inplace=True)
+
+        logger.info('Combining Description with why it\'s cult...')
+        why_its_cult = "Why It's Cult"
+        df['Description'] = df[[why_its_cult, 'Description']].apply(lambda x: f"Description:\n{x['Description']}\nWhy It's On SIIN:\n{x[why_its_cult]}", axis=1)
+        df.drop(why_its_cult, axis=1, inplace=True)
+
+        logger.info('Creating ships to bahrain column.')
+        df['ships_to_bahrain'] = df['Description'].transform(ship_to_bahrain)
+
+        logger.info('Removing regret message from description...')
+        df['Description'] = df['Description'].transform(remove_pattern)
+
+        logger.info('Removing brand name from product name...')
+        df['product_name'] = df[['brand_name', 'product_name']].apply(remove_brand_name, axis=1)
+
+        logger.info('Stripping all strings in data-frame...')
+        df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+
+
+        logger.info("Exporting excel without duplicates...")
+        df.to_excel('./test_cult_beauty_without_duplicates.xlsx', index=False)
+        logger.info('Total execution time: %s', datetime.timedelta(seconds=time.time() - start_time))
 
 if __name__ == '__main__':
     ACTION_DELAY_SEC = 1
+    JAVASCRIPT_EXECUTE_CLICK = "arguments[0].click();"
+    NUM_OF_WORKERS = 10
     browser_options = options.Options()
     browser_options.add_argument('-disable-notifications')
-    browser_options.add_experimental_option("prefs", {"profile.default_content_setting_values.cookies": 2})
-    browser_options.add_argument('-headless')
+    # browser_options.add_argument('-headless')
 
     color_variation_tags = [x.casefold() for x in ['colour:', 'color:']]
     shade_variation_tags = [x.casefold() for x in ['shade:']]
@@ -451,4 +550,5 @@ if __name__ == '__main__':
                     'https://www.cultbeauty.com/minis.list',
                     'https://www.cultbeauty.com/sale.list',
                     'https://www.cultbeauty.com/men.list']
+    logger.info('Scraping started.')
     main()
